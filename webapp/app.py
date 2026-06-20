6f6fec01b4646401ac9c4bf01d6f1737cdbaba9f
Tom Schmitz <108693424+schto173@users.noreply.github.com>
2026-06-21 00:30:35 +0200

    added 0000deploy

--- webapp/app.py
+++ webapp/app.py
@@ -153,5 +153,5 @@ app.mount("/static", StaticFiles(directory=STATIC), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    print("LAM Ecoquest Strategy Engine -> http://127.0.0.1:8000")
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="warning")
+    print("LAM Ecoquest Strategy Engine -> http://0.0.0.0:8000")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="warning")
